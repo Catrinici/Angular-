ac3f29a38960e85096e20ff4fc131acd9019ced3
Catrinici <catrinici.antonina@gmail.com>
2019-12-16 14:41:26 -0800

    first commit

---
+++ angularapp/src/app/components/user/user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-user',
+	templateUrl: './user.component.html'
+	//template: '<h2>John Doe</h2>'
+})
+
+export class UserComponent {
+
+}
