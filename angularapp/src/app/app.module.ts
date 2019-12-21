ae6df6211c50691f28f8361eea59c164048da880
Catrinici <catrinici.antonina@gmail.com>
2019-12-20 16:30:18 -0800

    first commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component'
+import { UserComponent } from './components/user/user.component';
+
 
 @NgModule({
-	declarations: [
-		AppComponent
-	],
-	imports: [
-		BrowserModule
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angularapp/src/app/components/user/user.component.ts
+++ angularapp/src/app/components/user/user.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-user',
-	templateUrl: './user.component.html'
-	//template: '<h2>John Doe</h2>'
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  // template: '<h2>John Doe</h2>'
+  // styles: [`
+  //   h2 {
+  //     color: blue
+  //   }
+  // `]
 })
 
 export class UserComponent {
